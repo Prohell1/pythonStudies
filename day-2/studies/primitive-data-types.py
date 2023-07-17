85626cd8ea7e40eacb3a0bdd7e4bddb55de01385
prohell1 <prohell@outlook.hu>
2023-07-17 13:24:34 +0200

    created primitive-data-types.py

---
+++ day-2/studies/primitive-data-types.py
@@ -0,0 +1,15 @@
+# Data Types
+# String
+print("Hello"[4])  # prints o
+print("123" + "345")  # prints 123345 (concatenation)
+
+# Integer
+print(123 + 345)  # prints 468 (the sum of the two integers)
+print(123_456_789)  # prints 123456789 (underscore is used for readability, but does not affect the interpreter)
+
+# Float
+3.14159  # floating point numbers have a decimal point, somewhere to identify it
+
+# Boolean
+True
+False
