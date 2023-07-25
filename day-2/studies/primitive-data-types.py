ef4f51ceaf9a1e15b386e03c6641931a352078e1
Gergely Kurucz <prohell@outlook.hu>
2023-07-25 12:36:50 +0200

    Added print statements to primitive-data-types.py

--- day-2/studies/primitive-data-types.py
+++ day-2/studies/primitive-data-types.py
@@ -8,8 +8,8 @@ print(123 + 345)  # prints 468 (the sum of the two integers)
 print(123_456_789)  # prints 123456789 (underscore is used for readability, but does not affect the interpreter)
 
 # Float
-3.14159  # floating point numbers have a decimal point, somewhere to identify it
+print(3.14159)  # floating point numbers have a decimal point, somewhere to identify it
 
 # Boolean
-True
-False
+print(True)
+print(False)
