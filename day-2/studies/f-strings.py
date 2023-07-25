1e6ffc6c0479e6c813dc60e1764712511a1e5e5a
Gergely Kurucz <prohell@outlook.hu>
2023-07-25 12:55:03 +0200

    created f-strings.py

---
+++ day-2/studies/f-strings.py
@@ -0,0 +1,7 @@
+score = 0
+height = 1.8
+isWinning = True
+
+print("Your score is " + str(score) + ", your height is " + str(height) + ", you are winning is " + str(isWinning) + ".")
+# using f-Strings, we don't need to concatenate variables together
+print(f'Your score is {score}, your height is {height}, you are winning is {isWinning}.')  # same result
