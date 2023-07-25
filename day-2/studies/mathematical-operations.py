ff5f128bffac9dd72fea7ff275409a9cb170a5b6
Gergely Kurucz <prohell@outlook.hu>
2023-07-25 12:31:41 +0200

    created mathematical-operations.py

---
+++ day-2/studies/mathematical-operations.py
@@ -0,0 +1,9 @@
+print(3 + 5)   # adding numbers - prints 8
+print(7 - 4)   # subtracting numbers - prints 3
+print(3 * 2)   # multiplying numbers - prints 6
+print(6 / 3)   # dividing numbers - prints 2.0 (result is always float)
+print(2 ** 3)  # raising the power of a number - prints 8
+
+# PEMDAS rule is applied, like in maths () => ** => * / => + -
+print(3 * 3 + 3 / 3 - 3)    # prints 7.0
+print(3 * (3 + 3) / 3 - 3)  # prints 3.0
