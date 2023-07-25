d01e67da36b849d940f7292da566195193e979eb
Gergely Kurucz <prohell@outlook.hu>
2023-07-25 12:51:54 +0200

    added floor division and rounding

--- day-2/studies/basic-mathematical-operations.py
+++ day-2/studies/basic-mathematical-operations.py
@@ -7,3 +7,13 @@ print(2 ** 3)  # raising the power of a number - prints 8
 # PEMDAS rule is applied, like in maths () => ** => * / => + -
 print(3 * 3 + 3 / 3 - 3)    # prints 7.0
 print(3 * (3 + 3) / 3 - 3)  # prints 3.0
+
+
+print(round(8 / 3, 2))  # prints 2.67, because it rounds the number to 2 decimal places
+print(8 // 3)           # prints 2, (floor division), result is always an integer
+
+
+result = 4 / 2  # saving the result to a variable
+result /= 2     # we reassign the result with a new number after the operation was made
+print(result)   # prints 1.0
+
