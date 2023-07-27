713f9d31da10c5e8a034cb19b8f7b91a7c91095c
Gergely Kurucz <prohell@outlook.hu>
2023-07-27 11:37:13 +0200

    added Section 4 studies

---
+++ day-4/studies/randomization.py
@@ -0,0 +1,12 @@
+import random
+
+randomInteger = random.randint(1, 10)
+print(randomInteger)
+
+# 0.000000 - 0.999999...
+randomFloat = random.random()
+print(randomFloat)
+
+loveScore = random.randint(1, 100)
+print(f"Your love score is {loveScore}")
+
