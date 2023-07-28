d7c20718f30d65ee84832a66c9e38f578e2c0f78
Gergely Kurucz <prohell@outlook.hu>
2023-07-28 13:44:00 +0200

    added studies

---
+++ day-6/studies/functions.py
@@ -0,0 +1,6 @@
+def my_function():  # Defining the function
+    print("Hello")  # Code block which needs to run when the function is called
+    print("Bye")
+
+
+my_function()  # Calling the function
