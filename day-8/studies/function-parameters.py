f352b646afdace3ecc3c74227d887eb614bc43fa
Gergely Kurucz <prohell@outlook.hu>
2023-07-29 10:49:19 +0200

    added day-8 studies

---
+++ day-8/studies/function-parameters.py
@@ -0,0 +1,24 @@
+# Simple Function
+# def greet():
+#     print("Hello!")
+#     print("How are you?")
+#     print("Nice to meet you!")
+# greet()
+
+# Function that allows for input
+# def greet_with_name(name):
+#     print(f"Hello {name}!")
+#     print(f"How are you, {name}?")
+# greet_with_name("Greg")
+
+# Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+
+
+# greet_with("Greg", "Budapest")
+
+greet_with(location="Budapest", name="Greg")
+
+
