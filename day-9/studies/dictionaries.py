5098028f1648bf037ebe3b203f25a4747204e5e9
Gergely Kurucz <prohell@outlook.hu>
2023-07-29 15:38:11 +0200

    added studies

---
+++ day-9/studies/dictionaries.py
@@ -0,0 +1,52 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+# Retrieving items from dictionary.
+print(programming_dictionary["Function"])
+
+# Adding new items to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+# Create an empty dictionary
+empty_dictionary = {}
+
+# Wipe an existing dictionary
+# programming_dictionary = {}
+
+# Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+
+# Loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+
+############################################
+
+# Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nesting a List in a Dictionary
+travel_log = {
+    "France": {"cities visited": ["Paris", "Lille", "Dijon"], "total visits": 12},
+    "Germany": {"cities visited": ["Berlin", "Hamburg", "Stuttgart"], "total visits": 5},
+}
+
+# Nesting Dictionary in a List
+travel_log = [
+    {
+        "Country": "France",
+        "Cities visited": ["Paris", "Lille", "Dijon"],
+        "total visits": 12
+    },
+    {
+        "Country": "Germany",
+        "Cities visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total visits": 5
+    },
+]
