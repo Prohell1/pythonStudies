b7256966c88ccbfedeafe6b23b609d80f70bdb60
Gergely Kurucz <prohell@outlook.hu>
2023-07-30 02:36:01 +0200

    updated with more studies

--- day-12/studies/scope.py
+++ day-12/studies/scope.py
@@ -1,14 +1,14 @@
 ################### Scope ####################
 
-enemies = 1
-
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")  # Prints 2
-
-
-increase_enemies()
-print(f"enemies outside function: {enemies}")  # Prints 1
+# enemies = 1
+#
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")  # Prints 2
+#
+#
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")  # Prints 1
 
 
 # Local Scope
@@ -21,13 +21,42 @@ print(f"enemies outside function: {enemies}")  # Prints 1
 # print(potion_strength)
 
 # Global Scope
-player_health = 10
+# player_health = 10
+#
+# def game():
+#     def drink_potion():
+#         potion_strength = 2
+#         print(player_health)
+#     drink_potion()
+#
+#
+# print(player_health)
 
-def game():
-    def drink_potion():
-        potion_strength = 2
-        print(player_health)
-    drink_potion()
+# There is no Block Scope
+
+# game_level = 3
+# def create_enemy():
+#     enemies = ["Skeleton", "Zombie", "Alien"]
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+#
+#     print(new_enemy)
+
+# Modifying Global Scope
+
+# enemies = 1
+# def increase_enemies():
+#     # global enemies
+#     print(f"enemies inside function: {enemies}")
+#     return enemies + 1
+#
+#
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# Global Constants
+
+PI = 3.14159
+URL = "https://www.google.com"
 
 
-print(player_health)
