48b348d701eaf6d802b2b22c75dfd780f112469a
Gergely Kurucz <prohell@outlook.hu>
2023-08-04 01:32:59 +0200

    updated with list comprehension

--- intermediate/day-25/us-states-game/main.py
+++ intermediate/day-25/us-states-game/main.py
@@ -10,7 +10,6 @@ turtle.shape(img)
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
-missing_states = []
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
@@ -26,11 +25,7 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
 
-for state in all_states:
-    if state in guessed_states:
-        continue
-    else:
-        missing_states.append(state)
+missing_states = [state for state in all_states if state not in guessed_states]
 
 new_data = pandas.DataFrame(missing_states)
 new_data.to_csv("states_to_learn.csv", header=["state"], index=False)
