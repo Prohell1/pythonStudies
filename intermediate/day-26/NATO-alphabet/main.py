40ed613ae354e837c95d212aacf1ef16d249311e
Gergely Kurucz <prohell@outlook.hu>
2023-08-08 18:18:41 +0200

    added exception handling

--- intermediate/day-26/NATO-alphabet/main.py
+++ intermediate/day-26/NATO-alphabet/main.py
@@ -4,7 +4,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # {new_key:new_value for (index, row) in df.iterrows()}
 alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-result = [alphabet[letter] for letter in word]
-print(result)
+is_input_invalid = True
+
+while is_input_invalid:
+    try:
+        word = input("Enter a word: ").upper()
+        result = [alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        is_input_invalid = False
+        print(result)
 
